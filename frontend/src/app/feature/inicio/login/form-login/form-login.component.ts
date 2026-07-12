1d628aa5a6e39ca88abb0bb604beaccede9b31ff
Ivan Flores <201900444@est.umss.edu>
2026-07-12 17:21:06 -0300

    Agregando FormBuilder y validaciones de frontend

--- frontend/src/app/feature/inicio/login/form-login/form-login.component.ts
+++ frontend/src/app/feature/inicio/login/form-login/form-login.component.ts
@@ -1,16 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './form-login.component.html',
   styleUrl: './form-login.component.css'
 })
 export class FormLoginComponent {
   selectedRole: 'estudiante' | 'docente' = 'estudiante';
+  private readonly router = inject(Router);
+  private readonly fb = inject(FormBuilder);
+  errorMessage = '';
+
+  loginForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(8)]]
+  })
 
   selectRole(role: 'estudiante' | 'docente'): void {
     this.selectedRole = role;
   }
+
+  onSubmit(){
+    let email = this.loginForm.value.email;
+    let password = this.loginForm.value.password;
+    let rol = this.selectedRole;
+
+    if(this.loginForm.valid){
+      console.log(`${email} - ${password} - ${rol}`);
+      this.router.navigate(['']);
+    }
+  }
+
+  get email(){return this.loginForm.get('email');}
+  get password(){return this.loginForm.get('password');}
 }
