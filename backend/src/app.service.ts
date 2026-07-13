b7685f1eb26c271a707c3ace0831bc0a652ed3b1
matias9486 <matias9486mdp@gmail.com>
2026-07-13 12:49:27 -0300

    T007: Instalación y configuración de bcrypt. Creación y uso de servicio con métodos para encriptar y comparar contraseña

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { CryptoService } from './crypto/crypto.service';
 
 @Injectable()
 export class AppService {
+  constructor(private readonly cryptoService: CryptoService) {}
+
   getHello(): string {
+    const passwordPlana = 'MiPasswordSegura123';
+
+    // Usamos el servicio inyectado
+    const passwordHasheada = this.cryptoService.hashPassword(passwordPlana);
+
+    console.log('--- PRUEBA CON CRYPTO SERVICE ---');
+    console.log('Original:', passwordPlana);
+    console.log('Hasheada:', passwordHasheada);
+    console.log('Comparación exitosa?', this.cryptoService.comparePassword(passwordPlana, passwordHasheada));
+    console.log('---------------------------------');
     return 'Hello World!';
   }
 }
