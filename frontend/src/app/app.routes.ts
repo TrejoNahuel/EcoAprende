260c3c4f5979f5ce93487baa6038b5eaab38964b
alex <alexpumari0@gmail.com>
2026-07-21 12:23:03 -0300

    Corregir rutas del frontend

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,9 +2,24 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
 import { FormLoginComponent } from './pages/inicio/form-login/form-login.component';
+import { FormRegisterComponent } from './pages/inicio/form-register/form-register.component';
 
 export const routes: Routes = [
-    {path: '', component: AuthLayoutComponent},
-    {path: 'home', component: HomeComponent},
-    {path: 'login', component: FormLoginComponent}
+    {
+        path: '',
+        component: HomeComponent,
+        pathMatch: 'full'
+    },
+    {
+        path: '', 
+        component: AuthLayoutComponent,
+        children: [
+            { path: 'login', component: FormLoginComponent },
+            { path: 'register', component: FormRegisterComponent }
+        ]
+    },
+    {
+        path: 'home', 
+        component: HomeComponent
+    },
 ];
