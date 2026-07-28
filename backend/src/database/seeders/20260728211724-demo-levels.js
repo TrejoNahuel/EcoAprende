f915a004a65983ea062d4cd1d8c8518872d00a37
Ivan Flores <201900444@est.umss.edu>
2026-07-28 18:22:31 -0300

    Seeder en Level

---
+++ backend/src/database/seeders/20260728211724-demo-levels.js
@@ -0,0 +1,63 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('level', [
+      {
+        id: 1,
+        name: 'Semilla',
+        minPoints: 0,
+      },
+      {
+        id: 2,
+        name: 'Nivel 2',
+        minPoints: 100,
+      },
+      {
+        id: 3,
+        name: 'Nivel 3',
+        minPoints: 300,
+      },
+      {
+        id: 4,
+        name: 'Nivel 4',
+        minPoints: 400,
+      },
+      {
+        id: 5,
+        name: 'Nivel 5',
+        minPoints: 500,
+      },
+      {
+        id: 6,
+        name: 'Nivel 6',
+        minPoints: 600,
+      },
+      {
+        id: 7,
+        name: 'Nivel 7',
+        minPoints: 700,
+      },
+      {
+        id: 8,
+        name: 'Nivel 8',
+        minPoints: 800,
+      },
+      {
+        id: 9,
+        name: 'Nivel 9',
+        minPoints: 900,
+      },
+      {
+        id: 10,
+        name: 'Brote',
+        minPoints: 1000,
+      },
+    ], {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('level', null, {});
+  }
+};
