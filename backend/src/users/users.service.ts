5d673809f95a3cb4421e22bfc4f67c26005918db
alex <alexpumari0@gmail.com>
2026-08-04 11:29:34 -0300

    Actualizar rama de feature/T048

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,8 +1,21 @@
 import type { ModelCtor } from 'sequelize-typescript';
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './models/user.model';
 import { InjectModel } from '@nestjs/sequelize';
+import { Level } from '../levels/models/level.model';
+
+export interface UserProfile {
+  id: number;
+  email: string;
+  role: string;
+  points: number;
+  level: {
+    id: number;
+    name: string;
+    minPoints: number;
+  }
+}
 
 @Injectable()
 export class UsersService {
@@ -10,16 +23,39 @@ export class UsersService {
     @InjectModel(User) private readonly userModel: ModelCtor<User>,
   ) {}
 
-  findByEmail(email: string): Promise<User | null> {
+  async findByEmail(email: string): Promise<User | null> {
     return this.userModel.findOne({ where: { email } });
   }
 
-  create({ email, password, role }: CreateUserDto): Promise<User> {
+  async create({ email, password, role }: CreateUserDto): Promise<User> {
     return this.userModel.create({
       email,
       password,
       role,
       points: 0,
     });
+  }
+
+  async getProfile(userId: number): Promise<UserProfile> {
+    const user = await this.userModel.findByPk(userId, {
+      include: Level,
+      attributes: { exclude: ['password'] },
+    });
+
+    if (!user) {
+      throw new NotFoundException('Usuario no encontrado');
+    }
+
+    return {
+      id: user.id,
+      email: user.email,
+      role: user.role,
+      points: user.points,
+      level: {
+        id: user.levelId,
+        name: user.level.name,
+        minPoints: user.level.minPoints,
+      }
+    };
   }
 }
