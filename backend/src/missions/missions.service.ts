89493a9b9591046fb6d95397dcfebde764444db0
@92377 <92377@sistemas.frc.utn.edu.ar>
2026-08-03 23:08:18 -0300

    a

--- backend/src/missions/missions.service.ts
+++ backend/src/missions/missions.service.ts
@@ -1,6 +1,10 @@
 import type { ModelCtor } from 'sequelize-typescript';
 import type { FindUserMissionsResponse } from './types/find-user-missions-response.type';
-import { Injectable, NotFoundException, ConflictException } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  ConflictException,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
 
 // 1. Importamos AMBOS modelos correctamente
@@ -12,26 +16,31 @@ export class MissionsService {
   constructor(
     // 2. Inyectamos los DOS modelos por separado y con sus nombres correctos
     @InjectModel(Mission) private readonly missionModel: ModelCtor<Mission>,
-    @InjectModel(UserMission) private readonly userMissionModel: typeof UserMission,
+    @InjectModel(UserMission)
+    private readonly userMissionModel: typeof UserMission,
   ) {}
 
   async findUserMissions(userId: number): Promise<FindUserMissionsResponse> {
     const completedMissions = await this.missionModel.findAll({
-      include: [{
-        model: UserMission,
-        attributes: [],
-        where: { userId },
-        required: true,
-      }],
+      include: [
+        {
+          model: UserMission,
+          attributes: [],
+          where: { userId },
+          required: true,
+        },
+      ],
     });
 
     const availableMissions = await this.missionModel.findAll({
-      include: [{
+      include: [
+        {
           model: UserMission,
           attributes: [],
           where: { userId },
           required: false,
-      }],
+        },
+      ],
       where: {
         '$userMissions.id$': null,
       },
@@ -56,7 +65,9 @@ export class MissionsService {
     });
 
     if (existingCompletion) {
-      throw new ConflictException('Ya has completado esta misión anteriormente');
+      throw new ConflictException(
+        'Ya has completado esta misión anteriormente',
+      );
     }
 
     // 3. Crear el registro de misión completada en la tabla intermedia
