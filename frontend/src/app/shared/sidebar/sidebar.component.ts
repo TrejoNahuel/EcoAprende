3a66752a08584d0912bdae99ad37c6df53d0152c
Ivan Flores <201900444@est.umss.edu>
2026-08-07 11:07:32 -0300

    Actualizando sidebar al modelo del main y separandolo en su componente

--- frontend/src/app/shared/sidebar/sidebar.component.ts
+++ frontend/src/app/shared/sidebar/sidebar.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 import { ProfileService, GetProfileResponse } from '../../services/profile.service';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
