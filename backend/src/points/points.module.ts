ca8fe8d7a2e6be2dd30e034a7c075048e9ba07f5
Ivan Flores <201900444@est.umss.edu>
2026-08-08 18:26:03 -0300

    Agregando points Controller - se hizo una prueba y todo funciona

--- backend/src/points/points.module.ts
+++ backend/src/points/points.module.ts
@@ -3,9 +3,11 @@ import { PointsService } from './points.service';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { Level } from 'src/levels/models/level.model';
 import { User } from 'src/users/models/user.model';
+import { PointsController } from './points.controller';
 
 @Module({
   imports: [SequelizeModule.forFeature([User, Level])],
-  providers: [PointsService]
+  providers: [PointsService],
+  controllers: [PointsController]
 })
 export class PointsModule {}
