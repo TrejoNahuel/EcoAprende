47be3a35bb6263917868a4ea2574e0b4b8ec37d3
@92377 <92377@sistemas.frc.utn.edu.ar>
2026-08-09 21:41:15 -0300

    feat(missions): implementar transacción y sumar puntos al completar misión (T047)

--- backend/src/missions/missions.module.ts
+++ backend/src/missions/missions.module.ts
@@ -4,9 +4,10 @@ import { MissionsController } from './missions.controller';
 import { Mission } from './models/mission.model';
 import { UserMission } from './models/user-mission.model';
 import { SequelizeModule } from '@nestjs/sequelize';
+import { PointsModule } from '../points/points.module';
 
 @Module({
-  imports: [SequelizeModule.forFeature([Mission, UserMission])],
+  imports: [SequelizeModule.forFeature([Mission, UserMission]), PointsModule],
   controllers: [MissionsController],
   providers: [MissionsService],
 })
